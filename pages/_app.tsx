0298f86a696376ffc0d6806bbbe21a5218bf2250
WorkAttackDev <workattackgeral@gmail.com>
2021-06-18 17:16:22 +0100

    4 files modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { HiMenuAlt3 } from "react-icons/hi";
+
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <>
+  <header>
+    <h1>
+      logo
+    </h1>
+    <HiMenuAlt3 />
+  </header>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <main>
-      <h1 className="text-gray-500">WorkAttack</h1>
+      
     </main>
   );
 }
